cfebdd7570afee6b7b43490f08461d56e2a6999d
WitherMite <43625516+WitherMite@users.noreply.github.com>
2023-12-08 14:07:38 -0600

    Make buttons write to display

--- calculator.js
+++ calculator.js
@@ -1,4 +1,44 @@
+const NUM_PAD = document.querySelectorAll(".numberBtn");
+const OPERATOR_BTNS = document.querySelectorAll(".operatorBtn");
+const ENTER_BTN = document.querySelector(".enterBtn");
+const CLEAR_BTN = document.querySelector(".clearBtn");
+const DISPLAY = document.querySelector(".calcDisplay");
 let firstNum, operator, secondNum;
+let displayContent = "";
+
+NUM_PAD.forEach(btn => {
+    btn.addEventListener("click", function () {
+        displayContent += this.textContent;
+        updateDisplay();
+    });
+});
+
+OPERATOR_BTNS.forEach(btn => {
+    btn.addEventListener("click", function () {
+        if (operator || !displayContent) return;
+        firstNum = displayContent;
+        displayContent = this.textContent + " "
+        operator = this.textContent;
+        updateDisplay();
+    });
+});
+
+CLEAR_BTN.addEventListener("click", () => {
+    firstNum = null;
+    operator = null;
+    secondNum = null;
+    clearDisplay();
+});
+
+function clearDisplay() {
+    displayContent = "";
+    updateDisplay();
+}
+
+function updateDisplay() {
+    DISPLAY.textContent = displayContent;
+    console.log(`${firstNum} ${operator} ${secondNum} | ${displayContent}`);
+}
 
 function operate(firstNum, operator, secondNum) {
     switch (operator) {
